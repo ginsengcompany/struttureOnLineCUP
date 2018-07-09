11b77ed7c268aecf52c796ddd1e62114ccc9c6d5
AntonioMona <antoniomona97@gmail.com>
2018-07-09 16:53:17 +0200

    Aggiunto campi per la registrazione

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+let bodyParser = require('body-parser');
 var logger = require('morgan');
 
 var routes = require('./routes/index');
@@ -24,8 +25,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
