9caf30af3ed02c29222f90494b71145fd54aad59
Luca Zito <lucazito@iMac-di-Luca.local>
2018-07-10 13:12:55 +0200

    aggiunto statocivile

--- public/javascript/registrazioneController.js
+++ public/javascript/registrazioneController.js
@@ -1,34 +1,51 @@
 $(document).ready(function () {
     $('.mdb-select').material_select();
     var selectProvinceNascita = $("#listaprovincenascita");
-    $('.mdb-select').material_select('destroy');
     $.ajax({
         type: "GET",
         url: "http://192.168.125.24:3001/comuni/listaprovince",
         dataType : "json",
         contentType : 'plain/text',
         success: function (data, textStatus, jqXHR) {
+            $('select[name="listaprovince"]').material_select('destroy');
             for (var i = 0; i < data.length; i++) {
                 selectProvinceNascita.append('<option value="' + data[i].codIstat + '">' + data[i].provincia + '</option>');
             }
-            $('.mdb-select').material_select();
+            $('select[name="listaprovince"]').material_select();
         },
         error: function(jqXHR, textStatus, errorThrown) {
             console.log(textStatus);
         }
     });
+    /*$('.mdb-select').material_select();
     var selectComuniNascita = $("#listacomunenascita");
     $('#listacomunenascita').material_select('destroy');
     $.ajax({
         type: "GET",
         url:"",
-        dataType:"",
-        contentType:"",
+        dataType:"json",
+        contentType:'plain/text',
         success: function (data, textStatus, jqXHR) {
             for (var i = 0; i < data.length; i++){
                 selectComuniNascita.append('<option value="' + data[i]. + '">' + data[i]. + '</option>');
             }
             $('.mdb-select').material_select();
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.log(textStatus);
+        }
+    });*/
+    var selectStatoCivile = $("#statocivile");
+    $.ajax({
+        type: "GET",
+        url:"http://192.168.125.24:3001/statocivile",
+        dataType:"json",
+        contentType:'plain/text',
+        success: function (data, textStatus, jqXHR) {
+            for (var i = 0; i < data.length; i++){
+                selectStatoCivile.append('<option value="' + data[i].id + '">' + data[i].descrizione + '</option>');
+            }
+            $('#statocivile').material_select();
         },
         error: function (jqXHR, textStatus, errorThrown) {
             console.log(textStatus);
