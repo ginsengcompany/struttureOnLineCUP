3262586784bbc6534e55d170ce30fc903057a8c7
AlessioRivoli <pleyboy87@hotmail.it>
2018-07-11 16:33:10 +0200

    RUBRICA QUASI FINITA MANCANO LE ROTTE

---
+++ public/javascript/rubricaController.js
@@ -0,0 +1,67 @@
+/* Formatting function for row details - modify as you need */
+function format ( d ) {
+    // `d` is the original data object for the row
+
+    return '<table   cellpadding="15" cellspacing="10" border="0.5" align="center"  style="padding-left:50px; width:100%;" >'+
+        '<tr>'+
+            '<th style="font-weight: bold">Codice Fiscale:</th>'+
+            '<td>'+d.codice_fiscale+'</td>'+
+            '<th style="font-weight: bold">Data di Nascita:</th>'+
+            '<td>'+d.data_nascita+'</td>'+
+            '<th style="font-weight: bold">Luogo di Nascita:</th>'+
+            '<td>'+d.luogo_nascita+'</td>'+
+        '</tr>'+
+        '<tr>'+
+            '<th style="font-weight: bold">Sesso:</th>'+
+            '<td>'+d.sesso+'</td>'+
+            '<th style="font-weight: bold">Comune Residenza:</th>'+
+            '<td>'+d.comune_residenza+'</td>'+
+            '<th style="font-weight: bold">Indirizzo Residenza:</th>'+
+            '<td>'+d.indirizzores+'</td>'+
+        '</tr>'+
+        '<tr>'+
+            '<th style="font-weight: bold">Telefono:</th>'+
+            '<td>'+d.telefono+'</td>'+
+            '<th style="font-weight: bold">Email:</th>'+
+            '<td>'+d.email+'</td>'+
+            '<th style="font-weight: bold">Extra info:</th>'+
+            '<td>'+d.prenota+'</td>'+
+        '</tr>'+
+        '</table>';
+}
+
+$(document).ready(function() {
+    var table = $('#example').DataTable( {
+
+        language: {
+            url: '../localisation/it-IT.json'
+        },
+        "ajax":"../javascript/modules/objects.txt",
+        "columns": [
+            {
+                "className":      'details-control',
+                "orderable":      false,
+                "data":           null,
+                "defaultContent": ''
+            },
+            { "data": "nome" },
+            { "data": "cognome" }
+        ],
+        "order": [[1, 'asc']]
+    } );
+    // Add event listener for opening and closing details
+    $('#example tbody').on('click', 'td.details-control', function () {
+        var tr = $(this).closest('tr');
+        var row = table.row( tr );
+        if ( row.child.isShown() ) {
+            // This row is already open - close it
+            row.child.hide();
+            tr.removeClass('shown');
+        }
+        else {
+            // Open this row
+            row.child( format(row.data()) ).show();
+            tr.addClass('shown');
+        }
+    } );
+} );
