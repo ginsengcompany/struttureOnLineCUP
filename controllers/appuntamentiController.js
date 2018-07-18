77ecc19b5ea794b9c6ae3f1b85e626626769cf54
aldominasi <aldo.minasi@gmail.com>
2018-07-18 11:52:59 +0200

    ho inserito la post per la gestione della lista appuntamenti dopo aver scelto l'assistito

--- controllers/appuntamentiController.js
+++ controllers/appuntamentiController.js
@@ -11,18 +11,46 @@ exports.getAppuntamenti = function (req, res, next) {
 };
 
 exports.getContatti = function (req,res,next) {
-    let options = {
-        method: 'GET',
-        uri: 'http://ecuptservice.ak12srl.it/auth/me',
-        headers:{
-            "x-access-token" : req.session.tkn
-        },
-        json : true
-    };
-    request(options,function (err, response, body) {
-        if(err)
-            return res.status(500).send("Il servizio è momentaneamente non disponibile");
-        res.status(response.statusCode).send(body);
+    if (strutture.db._readyState !== 1) return handleError({status: 500, message: "Il servizio è momentaneamente non disponibile"},res);
+    strutture.findOne({denominazioneUrl : req.params.azienda}, function (err, str) {
+        if (err) return handleError({status: 503, message: "Il servizio è momentaneamente non disponibile"},res);
+        if (!str) return handleError({status: 404, message: "Azienda Ospedaliera non trovata"},res);
+        let options = {
+            method: 'GET',
+            uri: 'http://ecuptservice.ak12srl.it/auth/me',
+            headers:{
+                "x-access-token" : req.session.tkn
+            },
+            json : true
+        };
+        request(options,function (err, response, body) {
+            if(err)
+                return res.status(500).send("Il servizio è momentaneamente non disponibile");
+            res.status(response.statusCode).send(body);
+        });
+    });
+};
+
+exports.getListaAppuntamentiAssistito = function (req, res) {
+    if (strutture.db._readyState !== 1) return handleError({status: 500, message: "Il servizio è momentaneamente non disponibile"},res);
+    strutture.findOne({denominazioneUrl : req.params.azienda}, function (err, str) {
+        if (err) return handleError({status: 503, message: "Il servizio è momentaneamente non disponibile"},res);
+        if (!str) return handleError({status: 404, message: "Azienda Ospedaliera non trovata"},res);
+        let options = {
+            method: 'POST',
+            uri: 'http://ecuptservice.ak12srl.it/auth/listaappuntamenti',
+            headers:{
+                "x-access-token" : req.session.tkn,
+                struttura:str.codice_struttura
+            },
+            body: req.body,
+            json : true
+        };
+        request(options,function (err, response, body) {
+            if(err)
+                return res.status(500).send("Il servizio è momentaneamente non disponibile");
+            res.status(response.statusCode).send(body);
+        });
     });
 };
 
