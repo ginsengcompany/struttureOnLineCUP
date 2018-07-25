ddabe15f12f4f73dbdf99d41101a6db9b8d37bad
aldominasi <aldo.minasi@gmail.com>
2018-07-25 16:18:46 +0200

    ho inserito il titolo in utte le pagine

--- controllers/nuovoContattoController.js
+++ controllers/nuovoContattoController.js
@@ -6,7 +6,7 @@ exports.getNuovoContatto = function (req, res, next) {
     strutture.findOne({denominazioneUrl : req.params.azienda}, function (err, str) {
         if (err) return handleError({status: 503, message: "Il servizio è momentaneamente non disponibile"},res);
         if (!str) return handleError({status: 404, message: "Azienda Ospedaliera non trovata"},res);
-        res.render('nuovoContatto',{datiAzienda:str,parametroAzienda:req.params.azienda});
+        res.render('nuovoContatto',{datiAzienda:str,parametroAzienda:req.params.azienda, pagetitle: "Nuovo contatto"});
     });
 };
 
