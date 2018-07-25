928f5ec21cd3d92fe7863683db609ae7cb3347a8
aldominasi <aldo.minasi@gmail.com>
2018-07-25 09:49:38 +0200

    ho commentato i js delle pagine registrazione e index

--- public/javascript/indexController.js
+++ public/javascript/indexController.js
@@ -1,17 +1,23 @@
+//rende invisibili i loghi
 $(".logo_login").hide();
+//rende invisibile la card della login
 $('#rowcard').hide();
 $(document).ready(function () {
+    //rende visibili e animati i loghi
     $('.logo-login').show();
     $('.logo-login').addClass('animated fadeInDown');
+    //dopo 1000 ms rende visibile e animata la card della login
     setTimeout(function () {
         $('#rowcard').show();
         $('#rowcard').addClass('animated rubberBand');
     }, 1000);
+    //assegna al submit della form la seguente callback
     $('#loginForm').submit(function () {
         eseguiLogin();
         return false;
     });
 });
+//esegue il tentativo di login dell'utente
 function eseguiLogin() {
     let parametriLogin ={
         username : String,
