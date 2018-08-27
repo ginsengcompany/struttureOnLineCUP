78102ba1ba0a9d6ce296b6aa4c4228c67d234fca
aldominasi <aldo.minasi@gmail.com>
2018-08-27 14:04:06 +0200

    ho inserito la rotta checkMe ed elimAccount in modo da poter eliminare l'account fornendo prima la password alla rotta checkMe che controlla tale campo e se questa è corretta la elimAccount applica il diritto all'oblio secondo il GDPR

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -42,17 +42,68 @@ exports.logout = function (req, res, next) {
 };
 
 exports.downloadMe = function (req, res, next) {
-    let options = {
-        method: 'GET',
-        uri: "http://localhost:3001/auth/downloadMe",
-        headers: {
-            "x-access-token" : req.session.tkn
-        }
-    };
-    request(options, function (err, response, body) {
-        if(err)
-            return res.status(500).send("Il servizio è momentaneamente non disponibile");
-        res.send(response.body);
+    if (strutture.db._readyState !== 1) return handleError({status: 500, message: "Il servizio è momentaneamente non disponibile"},res);
+    strutture.findOne({denominazioneUrl : req.params.azienda}, function (err, str) {
+        if (err) return handleError({status: 503, message: "Il servizio è momentaneamente non disponibile"}, res);
+        if (!str) return handleError({status: 404, message: "Azienda Ospedaliera non trovata"}, res);
+        let options = {
+            method: 'GET',
+            uri: "http://localhost:3001/auth/downloadMe",
+            headers: {
+                "x-access-token" : req.session.tkn
+            }
+        };
+        request(options, function (err, response, body) {
+            if(err)
+                return res.status(500).send("Il servizio è momentaneamente non disponibile");
+            res.send(response.body);
+        });
+    });
+};
+
+exports.elimAccount = function (req, res) {
+    if (strutture.db._readyState !== 1) return handleError({status: 500, message: "Il servizio è momentaneamente non disponibile"},res);
+    strutture.findOne({denominazioneUrl : req.params.azienda}, function (err, str) {
+        if (err) return handleError({status: 503, message: "Il servizio è momentaneamente non disponibile"}, res);
+        if (!str) return handleError({status: 404, message: "Azienda Ospedaliera non trovata"}, res);
+        let options = {
+            method: 'GET',
+            uri: "http://localhost:3001/auth/eliminaaccount",
+            headers:{
+                "x-access-token" : req.session.tkn
+            },
+            json : true
+        };
+        request(options,function (err, response, body) {
+            if(err)
+                return res.status(500).send("Il servizio è momentaneamente non disponibile");
+            delete req.session.auth;
+            res.status(response.statusCode).send(body);
+        });
+    });
+};
+
+exports.checkMe = function (req, res) {
+    if (strutture.db._readyState !== 1) return handleError({status: 500, message: "Il servizio è momentaneamente non disponibile"},res);
+    strutture.findOne({denominazioneUrl : req.params.azienda}, function (err, str) {
+        if (err) return handleError({status: 503, message: "Il servizio è momentaneamente non disponibile"}, res);
+        if (!str) return handleError({status: 404, message: "Azienda Ospedaliera non trovata"}, res);
+        let options = {
+            method: 'POST',
+            uri: "http://localhost:3001/auth/checkMe",
+            headers:{
+                "x-access-token" : req.session.tkn
+            },
+            body:{
+                password : req.body.password
+            },
+            json : true
+        };
+        request(options,function (err, response, body) {
+            if(err)
+                return res.status(500).send("Il servizio è momentaneamente non disponibile");
+            res.status(response.statusCode).send(body);
+        });
     });
 };
 
--- public/javascript/menu.js
+++ public/javascript/menu.js
@@ -37,3 +37,43 @@ $("#sitegesan").click(function () {
 $("#siteak12").click(function () {
     window.open("http://www.ak12srl.it","_blank");
 });
+
+$("#btnmodalElimAccountConferma").click(function () {
+    let passwordConferma = $("#modalPasswordElimAccount").val();
+    console.log(passwordConferma);
+    if (!passwordConferma || !passwordConferma.trim())
+        return;
+    console.log("oltre return");
+    $.ajax({
+        type: 'POST',
+        url: 'checkMe',
+        data: JSON.stringify({password: passwordConferma}),
+        dataType: "json",
+        contentType : 'application/json',
+        success: function (data, textStatus, jqXHR) {
+            $.ajax({
+                type: 'GET',
+                url: 'elimAccount',
+                dataType: "text",
+                success: function (data, textStatus, jqXHR) {
+                    $("#paragrafomodalNavbar").text("L'account è stato eliminato con successo, sarai reindirizzato alla pagina iniziale");
+                    $('#modalinfonavbar').modal('show');
+                    setTimeout(function () {
+                        window.location.href = 'login';
+                    }, 2000);
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    $("#paragrafomodalNavbar").text(jqXHR.responseText);
+                    $('#modalinfonavbar').modal('show');
+                    $("#modalPasswordElimAccount").val("");
+                }
+            });
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            $('#paragrafomodalNavbar').text(jqXHR.responseText);
+            $('#modalinfonavbar').modal('show');
+            $("#modalPasswordElimAccount").val("");
+        }
+    });
+
+});
