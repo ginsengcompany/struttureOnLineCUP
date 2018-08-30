884707138cbddbd309a29fa3c84935bab58d5933
aldominasi <aldo.minasi@gmail.com>
2018-08-30 14:51:06 +0200

    ho commentato il model della struttura

--- models/strutture.js
+++ models/strutture.js
@@ -1,29 +1,29 @@
 let mongoose = require('mongoose');
 let struttureSchema = new mongoose.Schema({
-    nome_struttura: String,
-    codice_struttura: String,
-    denominazioneUrl: String,
-    logo_struttura: String,
-    regione: String,
-    codRegione: String,
-    indirizzo: String,
-    piva: String,
+    nome_struttura: String, //nome della struttura
+    codice_struttura: String, //codice identificativo della struttura
+    denominazioneUrl: String, //denominazione della struttura (usato come parametro nell'url)
+    logo_struttura: String, //immagine della struttura
+    regione: String, //regione a cui appartiene la struttura
+    codRegione: String, //codice identificativo della regione
+    indirizzo: String, //indirizzo della struttura
+    piva: String, //partita iva
     contatto: {
-        numero_callCenter: String,
-        email: String
+        numero_callCenter: String, //numero telefonico del call center
+        email: String //posta elettronica della struttura
     },
-    pagamenti: Boolean,
-    referti: Boolean,
+    pagamenti: Boolean, //campo che indica se i pagamenti sono abilitati per la struttura
+    referti: Boolean, //campo che indica se i referti sono abilitati per la struttura
     variabili_logicaDati: {
-        onMoreReparti: Number,
-        onChangeAppuntamento: Number,
-        prenotazioniInBlocco: Number,
-        asl : Boolean,
-        host : String,
-        referti : String,
-        repositoryReferti : String
+        onMoreReparti: Number, //numero che indica come gestire il caso di più reparti per singola prestazione
+        onChangeAppuntamento: Number, //numero che indica come gestire il caso di appuntamento cambiato
+        prenotazioniInBlocco: Number, //numero che indica se i servizi della struttura gestiscono le prestazioni in blocco
+        asl : Boolean, //valore booleano che indica se la struttura è un asl
+        host : String, //host dei servizi della struttura
+        referti : String, //url del servizio per la lista dei referti
+        repositoryReferti : String //url del servizio per ottenere un referto
     },
-    url: {
+    url: { //url utilizzati dall'applicazione e-Cupt (url relativi ai servizi messi a disposizione dall'ecupt)
         TerminiServizio : String,
         Ricetta : String,
         StruttureErogatrici : String,
