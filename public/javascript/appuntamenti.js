4be5ce1fdfe9f99fc622ffd4c78a8c29a6b34f93
d4rk413x21 <darkalex21@gmail.com>
2018-09-07 12:40:04 +0200

    Aggiunto nascondi tabella durate annullamento

--- public/javascript/appuntamenti.js
+++ public/javascript/appuntamenti.js
@@ -200,6 +200,8 @@ $(document).ready(function () {
                                     $("#centralModalDanger").modal();
                                 }
                                 $("#btnAnnullaImpegnativa").click(function () {
+                                    $('#tableAppuntamenti').parents('div.dataTables_wrapper').first().hide();
+                                    $("#tableAppuntamenti").hide();
                                     $("#barra").show();
                                     $.ajax({
                                         type: "POST",
@@ -219,6 +221,7 @@ $(document).ready(function () {
                                     })
                                 });
                             });
+                            $('#tableAppuntamenti').parents('div.dataTables_wrapper').first().show();
                             $("#tableAppuntamenti").show();
                         }
                     },
